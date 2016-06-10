24e2629b71e47ce5b169e08cbcccc04e8f16e2dd
Alexander Jacks <alexanderjacks@Alexanders-MacBook-Air.local>
2016-06-10 14:24:00 -0700

    add partial function to business logic, using two arrays and not yet looping

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,26 @@
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    var arrayedInputs = [1, $('#input2').val()];
-    $('#result').append('<p>' + arrayedInputs + '<p>');
+    // var arrayNumbers = [1,playUntilNumber];
+    var playUntilNumber = $('#input2').val();
+    $('#result').append('<p>' + pingPongedInputs() + '<p>');
     $('#pong').show();
   });
 });
 
 // back-end logic
 
-// var pingPongedInputs = arrayedInputs.map(function(arrayedInput) {
-//   if ((arrayedInput % 15) = 0) {
-//     pingPongedInputs.push('ping-pong');
-//   } else if ((arrayedInput) % 5 = 0) {
+var arrayNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+var ppNumbers = [];
+var pingPongedInputs = (function(arrayNumber) {
+  if ((arrayNumber % 15) === 0) {
+    ppNumbers.push(15);
+  } else {
+    ppNumbers.push(0);
+  }
+});
+
+//   } else if ((arrayedInput % 5) === 0) {
 //     pingPongedInputs.push('pong');
-//   } else if ((arrayedInput) % 3 = 0) {
+//   } else if ((arrayedInput % 3) === 0) {
 //     pingPongedInputs.push('ping');
-//   } else {
-//     pingPongedInputs.push((arrayedInputs[i]));
-//   }
-// });
