9eeda333a04320d762f349c81edfb3ac204825ed
Alexander Jacks <alexanderjacks@Alexanders-MacBook-Air.local>
2016-06-10 10:01:21 -0700

    add initial backend logic to convert form input to integers, sum, and display result

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 // front-end logic
 $(document).ready(function() {
-  $("#ping").submit(function(event) {
+  $('#ping').submit(function(event) {
     event.preventDefault();
 
-    $("#pong").show();
-    $("#result").text("test output");
+    var resultText = function() {
+      return parseInt($('#input1').val()) + parseInt($('#input2').val());
+    };
+
+    $('#result').text(resultText);
+    $('#pong').show();
   });
 });
 
