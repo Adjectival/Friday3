8fb8ea2065b71b37cbb2e302b631326a077b1815
Alexander Jacks <alexanderjacks@Alexanders-MacBook-Air.local>
2016-06-10 13:50:59 -0700

    fix variable scope issue for input and slightly change front end code

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,21 @@
 // front-end logic
 $(document).ready(function() {
-  $('#ping').submit(function(event) {
+  $('form').submit(function(event) {
     event.preventDefault();
-
-    var arrayedInputs = [parseInt($('#input1').val()), parseInt($('#input2').val())];
-
-    var resultText = arrayedInputs;
-
-    // var resultText = function() {
-    //   // return
-    //   return parseInt($('#input1').val()) + parseInt($('#input2').val());
-    // };
-
-    $('#result').text(resultText);
+    var arrayedInputs = [1, $('#input2').val()];
+    $('#result').append('<p>' + arrayedInputs + '<p>');
     $('#pong').show();
   });
 });
 
 // back-end logic
-// var arrayedInputs = [];
+
 // var pingPongedInputs = arrayedInputs.map(function(arrayedInput) {
-//   if (arrayedInputs[i] % 15 = 0) {
+//   if ((arrayedInput % 15) = 0) {
 //     pingPongedInputs.push('ping-pong');
-//   } else if ((arrayedInputs[i]) % 5 = 0) {
+//   } else if ((arrayedInput) % 5 = 0) {
 //     pingPongedInputs.push('pong');
-//   } else if ((arrayedInputs[i]) % 3 = 0) {
+//   } else if ((arrayedInput) % 3 = 0) {
 //     pingPongedInputs.push('ping');
 //   } else {
 //     pingPongedInputs.push((arrayedInputs[i]));
