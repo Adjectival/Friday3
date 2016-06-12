9561bd5558c2be612db994335b44a99cf9db6789
Alexander Jacks <alexd.jacks@gmail.com>
2016-06-12 09:28:32 -0700

    add unfinished integration of all back end logic into one loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,30 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     // var arrayNumbers = [1,playUntilNumber];
-    // var playUntilNumber = $('#input2').val();
-    // $('#result').append('<p>' + pingPongedInputs(20) + '<p>');
+    // var playUntilNumber = $('#input').val();
+    // $('#result').append('<p>' + ppNums() + '<p>');
     $('#pong').show();
   });
 });
 
 // // back-end logic
 var playUntil = 12;
-var emptyNums = [];
+var inputNums = [1,2,3,4,5,6,7,15];
+var dittoNums  = inputNums.slice();
 
-var loopCount = 0;
-for (i=1; i<playUntil; i++) {
-  var loopCount=i;
-  var inputNums = emptyNums.push(loopCount);
-}
-
-// var inputNums = [2,3,4,5,6];
-// var ppNums = inputNums.map(function(inputNum) {
-//   if ((inputNum%15) === 0) {
-//     return ('ping-pong');
-//   } else if ((inputNum%5) === 0) {
-//     return ('pong');
-//   } else if ((inputNum%3) === 0) {
-//     return ('ping');
-//   } else {
-//     return (inputNum);
-//   }
-// });
+var ppNums = inputNums.map(function(inputNum) {
+  var loopCount = 0;
+  for (i=1; i<loopCount; i++) {
+    var loopCount=i;
+    if ((inputNum%15) === 0) {
+      console.log ('ping-pong');
+    } else if ((inputNum%5) === 0) {
+      console.log ('pong');
+    } else if ((inputNum%3) === 0) {
+      console.log ('ping');
+    } else {
+      console.log (inputNum);
+    }
+  }
+  // var dittoNum = inputNums.push(loopCount);
+});
