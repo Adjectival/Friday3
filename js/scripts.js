8094d089e39a6fd2b36eefcf6522f146ba69dddd
Alexander Jacks <alexd.jacks@gmail.com>
2016-06-11 20:58:16 -0700

    add functional loop that requires integration with existing code

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
-// This project is unfinished. I need to review quite a lot of material.
-// Will resubmit as soon as I can fulfill all requirements successfully.
-
 // front-end logic
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     // var arrayNumbers = [1,playUntilNumber];
-    var playUntilNumber = $('#input2').val();
-    $('#result').append('<p>' + pingPongedInputs(20) + '<p>');
+    // var playUntilNumber = $('#input2').val();
+    // $('#result').append('<p>' + pingPongedInputs(20) + '<p>');
     $('#pong').show();
   });
 });
 
-// back-end logic
-var upperBound = $('#input2').val();
-var arrayNumbers = [];
-for (i = 0; i < upperBound; i++){
-  arrayNumbers.push(1);
+// // back-end logic
+var playUntil = 18;
+var emptyNums = [];
+
+var loopCount = 0;
+for (i=1; i<playUntil; i++) {
+  loopCount+i;
+  var inputNums = emptyNums.push(playUntil);
 }
-var ppNumbers = [];
-var pingPongedInputs = (function(arrayNumber) {
-  arrayNumbers.forEach(function(arrayNumber) {
-    if ((arrayNumber % 15) === 0) {
-      ppNumbers.push('ping-pong');
-    } else if ((arrayNumber % 5) === 0) {
-      ppNumbers.push('pong');
-    } else if ((arrayNumber % 3) === 0) {
-      ppNumbers.push('ping');
-    } else {
-      ppNumbers.push(arrayNumber);
-    }
-  });
-});
+
+// var inputNums = [2,3,4,5,6];
+// var ppNums = inputNums.map(function(inputNum) {
+//   if ((inputNum%15) === 0) {
+//     return ('ping-pong');
+//   } else if ((inputNum%5) === 0) {
+//     return ('pong');
+//   } else if ((inputNum%3) === 0) {
+//     return ('ping');
+//   } else {
+//     return (inputNum);
+//   }
+// });
