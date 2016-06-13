4c94f4460602a738858302122968688b9ddfba4b
Alexander Jacks <alexd.jacks@gmail.com>
2016-06-12 20:21:05 -0700

    add simplified loop to back end, clean up HTML slightly

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,21 @@ var playUntil = 12;
 var inputNums = [1,2,3,4,5,6,7,15];
 var dittoNums  = inputNums.slice();
 
-var ppNums = inputNums.map(function(inputNum) {
-  var loopCount = 0;
-  for (i=1; i<loopCount; i++) {
-    var loopCount=i;
-    if ((inputNum%15) === 0) {
-      console.log ('ping-pong');
-    } else if ((inputNum%5) === 0) {
-      console.log ('pong');
-    } else if ((inputNum%3) === 0) {
-      console.log ('ping');
-    } else {
-      console.log (inputNum);
-    }
-  }
-  // var dittoNum = inputNums.push(loopCount);
-});
+// var ppNums = inputNums.map(function(inputNum) {
+
+var loopCount = 0;
+while (loopCount<playUntil) {
+  console.log('#',loopCount;)
+  loopCount=loopCount+1;
+  // if ((inputNum%15) === 0) {
+  //       console.log ('ping-pong');
+  //     } else if ((inputNum%5) === 0) {
+  //       console.log ('pong');
+  //     } else if ((inputNum%3) === 0) {
+  //       console.log ('ping');
+  //     } else {
+  //       console.log (inputNum);
+  //     }
+  //   }
+  // });
+}
