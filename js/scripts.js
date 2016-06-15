815af063da269646effbc4268dae850a8f304532
Alex Jacks <alexd.jacks@gmail.com>
2016-06-14 18:00:38 -0700

    use fixed values as test input, no ability to access branching logic yet, prints to screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,30 @@
 // // back-end logic
-var inputNumbers =[1,2,3,4,5,6,15,21];
-
+var inputNumbers =[1,2,3,5,6,15,21];
 var pingPongNumbers = inputNumbers.map(function(inputNumber) {
   return inputNumber*2;
 });
-alert(pingPongNumbers);
-
 var loopCount = 0;
+var playUntil = parseInt($("#until-number").val());
 while (loopCount<=playUntil) {
   console.log('#',loopCount);
   loopCount++;
-  if ((inputNum%15) === 0) {
-  // console.log ('ping-pong');
-  } else if ((inputNum%5) === 0) {
-  // console.log ('pong');
-  } else if ((inputNum%3) === 0) {
-  // console.log ('ping');
+  if ((inputNumber%15) === 0) {
+  console.log ('ping-pong');
+  } else if ((inputNumber%5) === 0) {
+  console.log ('pong');
+  } else if ((inputNumber%3) === 0) {
+  console.log ('ping');
   } else {
-  // console.log (inputNum);
+  console.log (inputNumbers);
   }
 }
-
 // front-end logic
-var playUntil = $("#until-number").val();
-
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
+    $(inputNumbers).val("#until-number");
     $('#pong').show();
-
-
-
-    $('#result').append('congratulations');
+    $('#result').text(inputNumbers[5]);
     // $('#result').append(' ' + pingPongNumbers() + ', ');
   });
 });
