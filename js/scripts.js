aea4909838fd8340e5818ea2c2ac87dae8b85fa9
Alex Jacks <alexd.jacks@gmail.com>
2016-06-14 17:43:13 -0700

    add working map method and rename variables for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
+// // back-end logic
+var inputNumbers =[1,2,3,4,5,6,15,21];
+
+var pingPongNumbers = inputNumbers.map(function(inputNumber) {
+  return inputNumber*2;
+});
+alert(pingPongNumbers);
+
+var loopCount = 0;
+while (loopCount<=playUntil) {
+  console.log('#',loopCount);
+  loopCount++;
+  if ((inputNum%15) === 0) {
+  // console.log ('ping-pong');
+  } else if ((inputNum%5) === 0) {
+  // console.log ('pong');
+  } else if ((inputNum%3) === 0) {
+  // console.log ('ping');
+  } else {
+  // console.log (inputNum);
+  }
+}
+
 // front-end logic
+var playUntil = $("#until-number").val();
+
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    // var arrayNumbers = [1,playUntilNumber];
-    // var playUntilNumber = $('#input').val();
-    // $('#result').append('<p>' + ppNums() + '<p>');
     $('#pong').show();
+
+
+
+    $('#result').append('congratulations');
+    // $('#result').append(' ' + pingPongNumbers() + ', ');
   });
 });
-
-// // back-end logic
-var playUntil = 12;
-var inputNums = [1,2,3,4,5,6,7,15];
-var dittoNums  = inputNums.slice();
-
-// var ppNums = inputNums.map(function(inputNum) {
-
-var loopCount = 0;
-while (loopCount<playUntil) {
-  console.log('#',loopCount;)
-  loopCount=loopCount+1;
-  // if ((inputNum%15) === 0) {
-  //       console.log ('ping-pong');
-  //     } else if ((inputNum%5) === 0) {
-  //       console.log ('pong');
-  //     } else if ((inputNum%3) === 0) {
-  //       console.log ('ping');
-  //     } else {
-  //       console.log (inputNum);
-  //     }
-  //   }
-  // });
-}
