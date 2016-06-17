10347bd919fe78374aafb69b431fe3698bc95204
Alexander Jacks <alexd.jacks@gmail.com>
2016-06-17 09:11:09 -0700

    use array to store pings and pongs from user input, then display to screen; fulfills project requirequirments

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 // back-end logic
-function pingPong(length) {
-  var ppArray = [];
-  for (var i=1; i<=length; i++) {
-    ppArray.push(i);
-  }
-  var output;
-  range.forEach(function(i) {
-    output = '';
-    if (i%15===0){
-			ppArray.push('ping-pong');
-		} else if(i%5===0){
-      ppArray.push('pong');
-		} else if(i%3===0) {
-      ppArray.push('ping');
-		} else {
-      output+=i;
+var ppArray = [];
+var pingPongIt = function(number) {
+  if (number>0) {
+    for (i=1; i<=number; i++) {
+      if (i%15===0) {
+      ppArray[i] = 'PING-PONG';
+      }
+      else if (i%5===0) {
+      ppArray[i] = 'pong';
+      }
+      else if (i%3===0) {
+      ppArray[i] = 'ping';
+      }
+      else {
+      ppArray[i] = i;
+      }
     }
-		print(output);
-	});
+    return ppArray;
+  }
 }
 // front-end logic
 $(document).ready(function() {
   $('form').submit(function(event) {
-    var playUntil = parseInt($('#until-number').val());
-    var result = ppArray;
-    $('#ppwords').show();
-    $('#result').text(result);
     event.preventDefault();
-    pingPong();
+    var playUntil = $('#until-number').val();
+    var result = pingPongIt(playUntil);
+    $('#result').text(result);
+    $('#ppwords').show();
   });
 });
